44081d6b43aa9368a678cf72362c6ab484633466
sima-coder <sihem.bellez@yahoo.fr>
2020-10-12 18:48:15 +0200

    Updates

---
+++ precache-manifest.314970b1e9fa5cf3afe3f95566b7703f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1435cd86e73c1048eee806efe634a0",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "67cd83e68c1cf3acbb32",
+    "url": "/todo-app/static/css/2.956e9ece.chunk.css"
+  },
+  {
+    "revision": "06cc2a3714fa6b7dafaa",
+    "url": "/todo-app/static/css/main.b402d53b.chunk.css"
+  },
+  {
+    "revision": "67cd83e68c1cf3acbb32",
+    "url": "/todo-app/static/js/2.32201d70.chunk.js"
+  },
+  {
+    "revision": "93028b05dda905f3b60d0775d9dda1ea",
+    "url": "/todo-app/static/js/2.32201d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06cc2a3714fa6b7dafaa",
+    "url": "/todo-app/static/js/main.1c94d2b7.chunk.js"
+  },
+  {
+    "revision": "d6223491b5542be29368",
+    "url": "/todo-app/static/js/runtime-main.ae733279.js"
+  },
+  {
+    "revision": "df41dce281c37f48d172f24a09aecc98",
+    "url": "/todo-app/static/media/img.df41dce2.jpg"
+  }
+]);
